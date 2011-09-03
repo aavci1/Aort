089b415704be99d5f4d992297e6720427bf30804
Abdurrahman AVCI <abdurrahmanavci@gmail.com>
2011-09-03 23:42:25 +0300

    OgreManager: Style fixes.

--- src/OgreManager.cpp
+++ src/OgreManager.cpp
@@ -128,32 +128,32 @@ Ogre::Entity *OgreManager::loadMesh(const QString &path) {
     // try to loading the mesh using assimp
     Assimp::Importer *importer = new Assimp::Importer();
     const aiScene *scene = importer->ReadFile(source.c_str(),
-                                              aiProcess_CalcTangentSpace |
-                                              aiProcess_JoinIdenticalVertices |
-                                              aiProcess_Triangulate |
-                                              // aiProcess_RemoveComponent |
-                                              // aiProcess_GenNormals |
-                                              aiProcess_GenSmoothNormals |
-                                              // aiProcess_SplitLargeMeshes |
-                                              // aiProcess_PreTransformVertices |
-                                              // aiProcess_LimitBoneWeights |
-                                              // aiProcess_ValidateDataStructure |
-                                              aiProcess_ImproveCacheLocality |
-                                              aiProcess_RemoveRedundantMaterials |
-                                              aiProcess_FixInfacingNormals |
-                                              aiProcess_SortByPType |
-                                              aiProcess_FindDegenerates |
-                                              aiProcess_FindInvalidData |
-                                              aiProcess_GenUVCoords |
-                                              aiProcess_TransformUVCoords |
-                                              // aiProcess_FindInstances |
-                                              aiProcess_OptimizeMeshes |
-                                              aiProcess_OptimizeGraph |
-                                              aiProcess_FlipUVs
-                                              // aiProcess_FlipWindingOrder |
-                                              // aiProcess_SplitByBoneCount |
-                                              // aiProcess_Debone
-                                              );
+                           aiProcess_CalcTangentSpace |
+                           aiProcess_JoinIdenticalVertices |
+                           aiProcess_Triangulate |
+                           // aiProcess_RemoveComponent |
+                           // aiProcess_GenNormals |
+                           aiProcess_GenSmoothNormals |
+                           // aiProcess_SplitLargeMeshes |
+                           // aiProcess_PreTransformVertices |
+                           // aiProcess_LimitBoneWeights |
+                           // aiProcess_ValidateDataStructure |
+                           aiProcess_ImproveCacheLocality |
+                           aiProcess_RemoveRedundantMaterials |
+                           aiProcess_FixInfacingNormals |
+                           aiProcess_SortByPType |
+                           aiProcess_FindDegenerates |
+                           aiProcess_FindInvalidData |
+                           aiProcess_GenUVCoords |
+                           aiProcess_TransformUVCoords |
+                           // aiProcess_FindInstances |
+                           aiProcess_OptimizeMeshes |
+                           aiProcess_OptimizeGraph |
+                           aiProcess_FlipUVs
+                           // aiProcess_FlipWindingOrder |
+                           // aiProcess_SplitByBoneCount |
+                           // aiProcess_Debone
+                                             );
     if (scene->HasMaterials()) {
       // TODO: load materials
     }
